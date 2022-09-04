5e1a634ebd3dc10322d94ad84e877de8bff8e0d8
Amir-H-Shzd <amirshir737@gmail.com>
2022-09-05 00:04:33 +0430

    set templates

--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+posts = [
+
+    {'author': 'Amir',
+        'title': 'first post',
+        'created_date': '4 september', },
+
+    {'author': 'Reza',
+        'title': 'seconed post',
+        'created_date': '5 september', }
+
+
+
+]
+
+
+def index(request):
+    # we use context to add data to our template
+    context = {'posts': posts}
+
+    return render(request, 'blog/index.html', context=context)
+
+
+def about(request):
+    # we send title from context to template to made some changes in title in tab of each page
+    return render(request, 'blog/about.html', {'title': 'About'})
---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+# Create your urls here.
+
+urlpatterns = [
+    path('', index, name='blog-home'),
+    path('about/', about, name='blog-about'),
+]
