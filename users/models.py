35d9ccbd0bc07d6f4e6fd8e919fcaa055f5f0048
Amir-H-Shzd <amirshir737@gmail.com>
2022-09-19 16:48:32 +0430

    update user profile

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
-from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 
@@ -16,5 +16,19 @@ class Profile(models.Model):
 
 # __str__ method help us to print the class with more details. in default it shows class.objects
 
+
     def __str__(self):
         return f'{self.user.username} Profile'
+
+
+# create a save method than resize the large photo than user uploaded befor saving
+
+    def save(self):
+        # with super class we can call a parent method like save to overwrite that
+        super().save()
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import UsersForm
+from .forms import UsersRegisterForm, UsersUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 # Create your views here.
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
-        form = UsersForm(request.POST)
+        form = UsersRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             # convert the information that users entered in to suitable format with cleaned_data
@@ -16,7 +16,7 @@ def register(request):
                 request, f'Thanks {username} for signing up :) please logged in to your account')
             return redirect('login')
     else:
-        form = UsersForm()
+        form = UsersRegisterForm()
 
     return render(request, 'users/register.html', {'form': form, 'title': 'Register'})
 
@@ -24,4 +24,20 @@ def register(request):
 # this decorator add functionality that user must be logged in to see themselves.
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html', {'title': 'Profile'})
+    if request.method == 'POST':
+        u_form = UsersUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid and p_form.is_valid:
+            u_form.save()
+            p_form.save()
+            messages.success(
+                request, f'Your profile has been successfully updated!')
+            return redirect('profile')
+    else:
+        u_form = UsersUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {'u_form': u_form, 'p_form': p_form, 'title': 'Profile'}
+
+    return render(request, 'users/profile.html', context)
